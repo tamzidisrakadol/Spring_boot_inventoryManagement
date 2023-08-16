123907d31637f55410b660f6494ca66ff40981d1
Tamzid Israk <tamzidisrak12@gmail.com>
2023-08-16 23:45:30 +0600

    introduction to inventory management

---
+++ src/main/java/com/example/Spring_boot_InventoryManager/Modal/Category.java
@@ -0,0 +1,52 @@
+package com.example.Spring_boot_InventoryManager.Modal;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "Products")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int categoryId;
+    String categoryName;
+    List<Product> productList;
+
+    public Category() {
+    }
+
+    public Category(int categoryId, String categoryName, List<Product> productList) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.productList = productList;
+    }
+
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+}
--- src/main/java/com/example/Spring_boot_InventoryManager/Modal/Product.java
+++ src/main/java/com/example/Spring_boot_InventoryManager/Modal/Product.java
@@ -1,4 +1,82 @@
 package com.example.Spring_boot_InventoryManager.Modal;
 
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    String name;
+    String description;
+    String quantity;
+    String expireDate;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, String description, String quantity, String expireDate) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.expireDate = expireDate;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(String expireDate) {
+        this.expireDate = expireDate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", quantity='" + quantity + '\'' +
+                ", expireDate='" + expireDate + '\'' +
+                '}';
+    }
 }
