dbf2ebf9b0186dd1e27cca57680785bf8e27b604
Tamzid Israk <tamzidisrak12@gmail.com>
2023-09-02 11:30:45 +0600

    implement add image functionality

--- src/main/java/com/example/Spring_boot_InventoryManager/Controller/AdminController.java
+++ src/main/java/com/example/Spring_boot_InventoryManager/Controller/AdminController.java
@@ -8,9 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 @Controller
 public class AdminController {
@@ -21,7 +26,7 @@ public class AdminController {
     @Autowired
     CategoryRepo categoryRepo;
 
-
+    // adding product html form
     @GetMapping("admin/addProduct")
     public String addProductui(Model model) {
         model.addAttribute("title", "Add product");
@@ -34,6 +39,7 @@ public class AdminController {
         return "admin/addProduct";
     }
 
+    // create category
     @PostMapping("/admin/createCategory")
     public String createCategory(@ModelAttribute("category") Category category) {
         Random random = new Random();
@@ -41,41 +47,56 @@ public class AdminController {
         category.setCategoryId(categoryRandomId);
         Category result = categoryRepo.save(category);
         System.out.println(result);
-        return "admin/addproduct";
+        return "redirect:/admin/addProduct";
     }
-    
 
+    // add product to desire category
     @PostMapping("/admin/update/{categoryId}")
-    public String updateCategoryList(@PathVariable("categoryId") int categoryId,@ModelAttribute("product") Product product){
+    public String updateCategoryList(@PathVariable("categoryId") int categoryId,
+            @ModelAttribute("product") Product product, @RequestParam("image") MultipartFile imageFile) {
+
         System.out.println(categoryId);
         Category category = productService.findByCategory(categoryId);
-        if (category!=null){
+        if (category != null) {
             Random random = new Random();
             int productRandomID = random.nextInt(150);
+            String imgurl = saveImage(imageFile);
+            product.setImgUrl(imgurl);
             product.setId(productRandomID);
             category.getProductList().add(product);
             productService.saveProduct(product);
             productService.saveCategory(category);
-            return "admin/showProduct";
-        }else{
+            return "redirect:/admin/addProduct";
+        } else {
             return "admin/addproduct";
         }
-        
+
     }
 
-    // to update the list
-    // @PutMapping("/admin/update/{id}")
-    // public String updateCategory(@PathVariable int id, @RequestBody Product
-    // product){
-    // Category category = productService.findByCategory(id);
-    // if (category!=null){
-    // category.getProductList().add(product);
-    // productService.saveProduct(product);
-    // return productService.saveCategory(category);
-    // }else{
-    // return null;
-    // }
+    private String saveImage(MultipartFile imageFile) {
 
-    // }
+        String uploadDirectory = "static/image";
+
+        String uniqueFileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
+
+        try {
+            var directory = new File(uploadDirectory);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            String filePath = uploadDirectory + uniqueFileName;
+            Files.write(Paths.get(filePath), imageFile.getBytes());
+            String imageUrl = "http://localhost:8080"+"/images/"+uniqueFileName;
+
+            return imageUrl;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        
+    }
 
 }
