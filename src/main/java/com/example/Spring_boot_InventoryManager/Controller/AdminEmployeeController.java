6a434504e78825ad76dea6dcb04df944f668f7ce
Tamzid Israk <tamzidisrak12@gmail.com>
2023-09-30 00:55:51 +0600

    update ui of add employee and create new page for adding employee category

--- src/main/java/com/example/Spring_boot_InventoryManager/Controller/AdminEmployeeController.java
+++ src/main/java/com/example/Spring_boot_InventoryManager/Controller/AdminEmployeeController.java
@@ -8,10 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminEmployeeController {
     
 
-
+    //add employee html form 
     @GetMapping("/admin/addEmployee")
     public String addEmployeeFormUI(Model model){
         model.addAttribute("title", "Add Employee");
         return "admin/employee/addEmployee";
+    }
+
+
+    //create employee category html form
+    @GetMapping("/admin/EmployeeCategory")
+    public String EmployeeCategory(Model model){
+        model.addAttribute("title", "Create Categpry");
+        return "admin/employee/createEmployeeCategory";
     }
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -3,21 +3,21 @@ const inputFile = document.querySelector('#file');
 const imageArea = document.querySelector('.image-area')
 
 
-selectImage.addEventListener('click' , function(){
+selectImage.addEventListener('click', function () {
     inputFile.click();
 })
 
 
-inputFile.addEventListener('change', function(){
+inputFile.addEventListener('change', function () {
     const image = this.files[0];
     console.log(image);
     const reader = new FileReader();
-    reader.onload = ()=> {
+    reader.onload = () => {
         const allimg = imageArea.querySelectorAll('img')
-        allimg.forEach(item=> item.remove());
-        const imgUrl=reader.result;
+        allimg.forEach(item => item.remove());
+        const imgUrl = reader.result;
         const img = document.createElement('img')
-        img.src=imgUrl
+        img.src = imgUrl
         imageArea.appendChild(img)
         imageArea.classList.add('active');
     }
@@ -31,7 +31,11 @@ function updateFormAction() {
 }
 
 function redirectToAnotherPage() {
-   
-    window.location.href =  "/admin/category";
+
+    window.location.href = "/admin/category";
 }
 
+function redirectToEmployeeCategoryPage() {
+
+    window.location.href = "/admin/EmployeeCategory";
+}
