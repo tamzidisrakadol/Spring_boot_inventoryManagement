7d2e71a09a4bb01e961c21c9285b5c65bfd6bacf
Tamzid Israk <tamzidisrak12@gmail.com>
2023-10-01 23:28:43 +0600

    add functionality of creating employee category

--- src/main/java/com/example/Spring_boot_InventoryManager/Controller/AdminEmployeeController.java
+++ src/main/java/com/example/Spring_boot_InventoryManager/Controller/AdminEmployeeController.java
@@ -1,25 +1,45 @@
 package com.example.Spring_boot_InventoryManager.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.Spring_boot_InventoryManager.Modal.EmployeeCategory;
+import com.example.Spring_boot_InventoryManager.Service.EmployeeService;
 
 @Controller
 public class AdminEmployeeController {
-    
 
-    //add employee html form 
+    @Autowired
+    EmployeeService employeeService;
+
+    // add employee html form
     @GetMapping("/admin/addEmployee")
-    public String addEmployeeFormUI(Model model){
+    public String addEmployeeFormUI(Model model) {
         model.addAttribute("title", "Add Employee");
+        List<EmployeeCategory> categoriyList = employeeService.showEmployeeCategory();
+        model.addAttribute("categoriyList", categoriyList);
         return "admin/employee/addEmployee";
     }
 
-
-    //create employee category html form
+    //  employee category html form
     @GetMapping("/admin/EmployeeCategory")
-    public String EmployeeCategory(Model model){
+    public String EmployeeCategory(Model model) {
         model.addAttribute("title", "Create Categpry");
+        EmployeeCategory employeeCategory = new EmployeeCategory();
+        model.addAttribute("employeeCategory", employeeCategory);
         return "admin/employee/createEmployeeCategory";
+    }
+
+    //create employee category
+    @PostMapping("/admin/createEmployeeCategory")
+    public String createEmployeeCategory(@ModelAttribute("employeeCategory") EmployeeCategory employeeCategory) {
+        employeeService.createEmployeeCategory(employeeCategory);
+        return "redirect:/admin/addEmployee";
     }
 }
--- src/main/java/com/example/Spring_boot_InventoryManager/Service/EmployeeService.java
+++ src/main/java/com/example/Spring_boot_InventoryManager/Service/EmployeeService.java
@@ -1,11 +1,16 @@
 package com.example.Spring_boot_InventoryManager.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+import com.example.Spring_boot_InventoryManager.Modal.EmployeeCategory;
 import com.example.Spring_boot_InventoryManager.Repository.EmployeeCategoryRepo;
 import com.example.Spring_boot_InventoryManager.Repository.EmployeeRepo;
 
+
+@Service
 public class EmployeeService {
     
 
@@ -14,5 +19,16 @@ public class EmployeeService {
 
     @Autowired
     EmployeeCategoryRepo employeeCategoryRepo;
+
+
+    public String  createEmployeeCategory(EmployeeCategory employeeCategory){
+        employeeCategoryRepo.save(employeeCategory);
+        return "success";
+    }
+
+
+    public List<EmployeeCategory> showEmployeeCategory(){
+        return employeeCategoryRepo.findAll();
+    }
 
 }
